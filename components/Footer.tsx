6c62e281354dd5ae744c67f6acf0d743e0df2a11
RohabAmir <rohabamir2@gmail.com>
2024-06-01 15:32:48 +0500

    Fixes Bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,10 +5,7 @@ import { socialMedia } from '@/data';
 
 const Footer = () => {
   return (
-    <footer className='w-full pt-20 pb-10' id='contact'>
-        <div className='w-full absolute left-0 -bottom-72 min-h-96'>
-            <img src='/footer-grid.svg' alt='footerGridBg' className='w-full h-full opacity-50' />
-        </div>
+    <footer className='w-full  pb-10 mb-[100px] md:mb-5' id='contact'>
         <div className='flex items-center flex-col'>
             <h1 className='heading lg:max-w-45[vw]'>
                 Ready to take <span className='text-purple'>your</span> digital presence to the next level?
@@ -25,7 +22,7 @@ const Footer = () => {
             </a>
         </div>
         <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
+        <p className="md:text-base text-sm md:font-normal font-light mb-5 md:mb-0">
           Copyright © 2024 Rohab Aamir
         </p>
 
