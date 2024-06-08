19aa8e5224577aad42cb1085e0388cc175b7d954
RohabAmir <rohabamir2@gmail.com>
2024-06-09 02:52:44 +0500

    Magic button css adjustments for ios devices

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -20,12 +20,7 @@ const MagicButton = ({
       }}
     >
       <span 
-        className="absolute inset-[-1000%] "
-        style={{
-          backgroundImage: 'conic-gradient(from 90deg at 50% 50%, #E2CBFF 0%, #393BB2 50%, #E2CBFF 100%)',
-          animation: 'spin 2s linear infinite',
-          WebkitAnimation: 'spin 2s linear infinite', // for Safari
-        }}
+        className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"
       />
       <span 
         className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white gap-2 ${position === 'left' ? 'flex-row-reverse' : ''}`}
