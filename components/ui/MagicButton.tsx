1722eaa2f338ba38b5f57ba4991a23f1119bd6c2
RohabAmir <rohabamir2@gmail.com>
2024-06-09 02:09:00 +0500

    Magic button css adjustments for ios devices

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,19 +1,39 @@
+
 import React from "react";
 
 const MagicButton = ({
     title, icon, position, handleClick, otherClasses
 }:{
     title: string;
-    icon: React.ReactNode
+    icon: React.ReactNode;
     position: string;
     handleClick?: () => void;
     otherClasses?: string;
-
 }) => {
   return (
-    <button className="relative inline-flex h-12 w-full overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10" onClick={handleClick}>
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-      <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}>
+    <button 
+      className={`relative inline-flex h-12 w-full overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10 ${otherClasses}`}
+      onClick={handleClick}
+      style={{
+        // Fallback for iOS where conic-gradient might not be supported
+        background: 'radial-gradient(circle, #E2CBFF, #393BB2)'
+      }}
+    >
+      <span 
+        className="absolute inset-[-1000%] "
+        style={{
+          backgroundImage: 'conic-gradient(from 90deg at 50% 50%, #E2CBFF 0%, #393BB2 50%, #E2CBFF 100%)',
+          animation: 'spin 2s linear infinite',
+          WebkitAnimation: 'spin 2s linear infinite', // for Safari
+        }}
+      />
+      <span 
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white gap-2 ${position === 'left' ? 'flex-row-reverse' : ''}`}
+        style={{
+          backdropFilter: 'blur(16px)',
+          WebkitBackdropFilter: 'blur(16px)', // for Safari
+        }}
+      >
         {position === "left" && icon}
         {title}
         {position === "right" && icon}
